2246ee19de05b2351f0d54214762495fe026e9f0
Hakan Erdem Temiz <42352651+hakan-erdem-temiz@users.noreply.github.com>
2020-07-08 12:02:32 +0300

    auth added

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -23,15 +23,26 @@ interface UserDoc extends mongoose.Document {
 
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    }
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON : {
+      transform(doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+        delete ret.password;
+        delete ret.__v
+      }
+    }
+  }
+);
 
 //HOOK
 // every time you save data to db this middleware will run (this is mongodb middleware)
